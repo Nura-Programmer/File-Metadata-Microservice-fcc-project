69c16aba77a1dc767d63d8cd742c25348674a185
Nura Programmer <53842620+Nura-Programmer@users.noreply.github.com>
2022-07-22 09:08:09 +0100

    add index.js with top level route and error handler

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+// Error Handler
+app.use((req, res, next) => {
+  const error = (new Error('Not Found').status = 404);
+  next(error);
+});
+
+// Error route
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    error: err.message || 'Internal Server Error',
+  });
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
