3bf1b76a2d1d1feae6b4e143a9af6acefb6e33e3
Nura Programmer <53842620+Nura-Programmer@users.noreply.github.com>
2022-07-22 09:57:16 +0100

    use express-fileupload dependency

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -9,10 +10,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(fileUpload()); // use default options
 
 // Routes
 app.get('/', (req, res) => {
   res.sendFile('index.html');
+});
+
+app.post('/api/fileanalyse', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0)
+    return res.status(400).send('No files were uploaded.');
+
+  const { name, mimetype, size } = req.files.myFile;
+
+  const respondObj = { name, type: mimetype, size };
+
+  console.log(respondObj);
+  res.json(respondObj);
 });
 
 // Error Handler
